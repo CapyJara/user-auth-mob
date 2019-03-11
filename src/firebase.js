2f73bd383f5358db1e54f323f28f6861e81f9507
UncleJerry <jared.myhrberg@gmail.com>
2019-03-11 11:58:12 -0700

    initialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+    // Initialize Firebase
+const config = {
+    apiKey: 'AIzaSyCAmmIqePd22ElnUSmZUb13g0jdeRz2-k8',
+    authDomain: 'user-auth-mob-715e4.firebaseapp.com',
+    // databaseURL: 'https://user-auth-mob-715e4.firebaseio.com',
+    // projectId: 'user-auth-mob-715e4',
+    // storageBucket: 'user-auth-mob-715e4.appspot.com',
+    // messagingSenderId: '67611161742'
+};
+firebase.initializeApp(config);
+
