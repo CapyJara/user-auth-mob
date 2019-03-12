88dd1bf6fe568d2e2b81997e603cb8dd078c4414
UncleJerry <jared.myhrberg@gmail.com>
2019-03-12 13:33:26 -0700

    add users db to auth

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,5 +1,5 @@
 import loadHeader from '../header-component.js';
-import { auth } from './firebase.js';
+import { auth, usersRef } from './firebase.js';
 
 loadHeader({ skipAuth: true });
 
@@ -11,5 +11,17 @@ ui.start('#auth-container', {
         firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     signInSuccessUrl: './',
-    credentialHelper: firebaseui.auth.CredentialHelper.NONE
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    callbacks: {
+        signInSuccessWithAuthResult(authResult) {
+            const user = authResult.user;
+            usersRef.child(user.uid)
+                .set({
+                    uid: user.uid,
+                    displayName: user.displayName,
+                    photoURL: user.photoURL
+                });
+            return true;
+        }
+    }
 });
