a75e819266d10837a618d5c0af3c6d30573b575f
UncleJerry <jared.myhrberg@gmail.com>
2019-03-12 11:45:08 -0700

    added database url and project id to config in firebase js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,8 @@
 const config = {
     apiKey: 'AIzaSyCAmmIqePd22ElnUSmZUb13g0jdeRz2-k8',
-    authDomain: 'user-auth-mob-715e4.firebaseapp.com'
+    authDomain: 'user-auth-mob-715e4.firebaseapp.com',
+    databaseURL: 'https://user-auth-mob-715e4.firebaseio.com',
+    projectId: 'user-auth-mob-715e4'
 };
 firebase.initializeApp(config);
 
